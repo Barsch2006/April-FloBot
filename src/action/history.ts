7f348937c7b4a67de981ae66be87970d995ab8c8
Barsch2006 <cchr98756@gmail.com>
2023-03-18 20:55:57 +0100

    update fake-history & spaß timeout

--- src/action/history.ts
+++ src/action/history.ts
@@ -1,3 +1,4 @@
+import { randomInt } from 'crypto';
 import { Client, CommandInteraction, EmbedBuilder, GuildMember, Colors } from "discord.js"
 import { AsyncDatabase } from '../sqlite/sqlite'
 import { ILogger } from '../logger/logger'
@@ -6,60 +7,27 @@ export default async function (client: Client, interaction: CommandInteraction, 
   const target = interaction.options.getMember('target') as GuildMember
 
   try {
-    const resultban = await db.allAsync(`SELECT * FROM records WHERE dc_id = ? AND type = ?`, [target.id, 'BAN'])
-    const resultkick = await db.allAsync(`SELECT * FROM records WHERE dc_id = ? AND type = ?`, [target.id, 'KICK'])
-    const resultstrike = await db.allAsync(`SELECT * FROM records WHERE dc_id = ? AND type = ?`, [target.id, 'STRIKE'])
-    const resultwarn = await db.allAsync(`SELECT * FROM records WHERE dc_id = ? AND type = ?`, [target.id, 'WARN'])
-    const result = await db.allAsync(`SELECT points FROM records WHERE dc_id = ?`, [target.id])
-    let gespoints = 0
-    result.forEach((row) => {
-      gespoints = gespoints + parseInt(row?.points)
-    })
+    const resultban = randomInt(1000);
+    const resultkick = randomInt(1000);
+    const resultstrike = randomInt(1000);
+    const resultwarn = randomInt(1000);
+    const gespoints = resultban + resultkick + resultstrike + resultwarn
     const dataEmbed = new EmbedBuilder()
       .setTitle(`${target.displayName}`)
       .setThumbnail(`${target.displayAvatarURL()}`)
       .setDescription(
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/restrict-plus-operands
         `Joined: ${target.joinedAt?.getDate()}.${(target.joinedAt?.getMonth() ?? 0) + 1}.${target.joinedAt?.getFullYear()}
-        Anzahl Banns: ${resultban.length}
-        Anzahl Kicks: ${resultkick.length}
-        Anzahl Strikes: ${resultstrike.length}
-        Anzahl Warns: ${resultwarn.length}
+        Anzahl Banns: ${resultban}
+        Anzahl Kicks: ${resultkick}
+        Anzahl Strikes: ${resultstrike}
+        Anzahl Warns: ${resultwarn}
         Anzahl Points: ${gespoints}
         `
       )
       .setColor(Colors.Yellow)
       .setTimestamp()
-    dataEmbed.addFields({ name: '\u200b', value: '__Kicks__' })
-    resultkick.forEach(row => {
-      dataEmbed.addFields(
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Grund:`, value: `\b ${row?.reason}` }
-      )
-    })
-    dataEmbed.addFields({ name: '\u200b', value: '__Strikes__' })
-    resultstrike.forEach(row => {
-      dataEmbed.addFields(
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Grund:`, value: `\b ${row?.reason}` }
-      )
-    })
-    dataEmbed.addFields({ name: '\u200b', value: '__Warns__' })
-    resultwarn.forEach(row => {
-      dataEmbed.addFields(
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Grund:`, value: `\b ${row?.reason}` }
-      )
-    })
-    // Banns
-    /* dataEmbed.addFields({ name: '\u200b', value: '__Banns__' })
-    resultban.forEach(row => {
-      dataEmbed.addFields(
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        { name: `Grund:`, value: `\b ${row?.reason}` }
-      )
-    }) */
-    logger.logSync("INFO", `History from ${target.id}`)
+    logger.logSync("INFO", `Fake-History from ${target.id}`)
     await interaction.reply({
       embeds: [
         dataEmbed
--- src/action/timeout.ts
+++ src/action/timeout.ts
@@ -1,5 +1,6 @@
 import { Client, Colors, CommandInteraction, EmbedBuilder, GuildMember } from 'discord.js'
-import { AsyncDatabase } from 'src/sqlite/sqlite'
+import ms from 'ms'
+import { AsyncDatabase } from '../sqlite/sqlite'
 import { ILogger } from '../logger/logger'
 
 export default async (client: Client, interaction: CommandInteraction, logger: ILogger, db: AsyncDatabase): Promise<void> => {
@@ -8,7 +9,27 @@ export default async (client: Client, interaction: CommandInteraction, logger: I
     return
   }
 
-  await interaction.reply({
-    embeds: [new EmbedBuilder().setDescription('Timeout fehlgeschlagen')], ephemeral: false
-  })
+  const reason = interaction.options.get('reason', true).value?.toString() ?? ''
+  var member = interaction.member as GuildMember
+  const user = interaction.options.getMember('target') as GuildMember;
+  // member = user
+  const zeit = ms(interaction.options.get('zeit')?.value as string)
+
+  const embed = new EmbedBuilder()
+    .setTitle('User hat einen Timeout bekommen')
+    .setDescription(`<@${member.id}> hat einen Timeout bekommen.`)
+    .setColor(Colors.Purple)
+    .addFields({ name: 'Grund', value: `${member.user.username} hat versucht ${user.user.username} zu timeouten.` })
+    .setAuthor({ name: `Originaler Grund: ${reason}` })
+    .setTimestamp()
+
+  try {
+    await member?.timeout(zeit, `${member.user.username} hat versucht ${user.user.username} zu timeouten.`)
+    await interaction.reply({ embeds: [embed], ephemeral: false })
+  } catch (err) {
+    logger.logSync('ERROR', `Timeout konnte nicht ausgefuehrt werden. ${JSON.stringify(err)}`)
+    await interaction.reply({
+      embeds: [new EmbedBuilder().setDescription('Timeout fehlgeschlagen')], ephemeral: false
+    })
+  }
 }
